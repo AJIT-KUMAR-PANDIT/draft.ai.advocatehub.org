a01d18475117f33b07743f72ccad719930d76321
AJIT-KUMAR-PANDIT <ajitkumarpandit1999@gmail.com>
2026-04-29 15:35:15 +0530

    iup

---
+++ app/ai/page.tsx
@@ -0,0 +1,15 @@
+
+"use client";
+
+import React from "react";
+import AI_Prompt from "@/components/kokonutui/ai-prompt";
+
+const AI = () => {
+  return (
+    <div className="h-screen flex items-center justify-center p-50 w-full">
+      <AI_Prompt />
+    </div>
+  );
+};
+
+export default AI;
