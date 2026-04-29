34fd359d3552951f6e09c96bf78dbd3afc7a855d
AJIT-KUMAR-PANDIT <ajitkumarpandit1999@gmail.com>
2026-04-29 15:40:01 +0530

    Z

--- app/ai/page.tsx
+++ app/ai/page.tsx
@@ -6,7 +6,7 @@ import AI_Prompt from "@/components/kokonutui/ai-prompt";
 
 const AI = () => {
   return (
-    <div className="h-screen flex items-center justify-center p-50 w-full">
+    <div className="h-screen flex items-center justify-center w-full">
       <AI_Prompt />
     </div>
   );
